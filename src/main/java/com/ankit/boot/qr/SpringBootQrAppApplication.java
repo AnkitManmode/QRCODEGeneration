2a69bb2713547be76de99538efca6fd356593854
AnkitManmode <ankitmanmode123@gmail.com>
2023-01-04 16:33:25 +0530

    QR code Generation
    Its all about QR code generation by using spring boot

---
+++ src/main/java/com/ankit/boot/qr/SpringBootQrAppApplication.java
@@ -0,0 +1,15 @@
+package com.ankit.boot.qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQrAppApplication.class, args);
+	
+		System.out.println("application success");
+	}
+
+}
---
+++ src/test/java/com/ankit/boot/qr/SpringBootQrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.boot.qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
